fea5eb0b1e43b879ebbfc846ffaf5504ef09a5cf
yanchangyou <yanchangyou@gmail.com>
2014-08-30 20:02:06 +0800

    helloworld-java-jdbc 1, 提交初版：包括mysql和oracle的连接例子

---
+++ src/org/code/helloworld/java/jdbc/mysql/MySql.java
@@ -0,0 +1,68 @@
+package org.code.helloworld.java.jdbc.mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
+/**
+ * 
+ * 描述：mysql 连接数据库测试
+ * 
+ * @author yanchangyou@gmail.com
+ * @date 2014-08-30 19:55:57
+ *
+ */
+public class MySql {
+
+	public void test() throws Exception {
+
+		// 加载驱动
+		Class.forName("com.mysql.jdbc.Driver");
+
+		// 数据库连接信息：url，用户名，密码
+		String url = "jdbc:mysql://127.0.0.1:3306/test";
+		String userName = "root";
+		String password = "";
+
+		// 获取连接
+		Connection conn = DriverManager.getConnection(url, userName, password);
+
+		// 创建语句对象
+		Statement stat = conn.createStatement();
+
+		// 查询语句
+		String sql = "SELECT * FROM information_schema.TABLES";
+
+		// 执行查询
+		ResultSet rs = stat.executeQuery(sql);
+		ResultSetMetaData rsMeta = rs.getMetaData();
+
+		// 输出列名称
+		int columns = rsMeta.getColumnCount();
+		for (int i = 0; i < columns; i++) {
+			System.out.print(rsMeta.getColumnName(i + 1) + "\t");
+		}
+
+		System.out.println();
+		System.out.println();
+
+		while (rs.next()) {// 遍历每一行
+
+			// 输出每行的列值
+			for (int i = 0; i < columns; i++) {
+				Object value = rs.getObject(i + 1);
+
+				// 输出每列的列值
+				System.out.print(value + "\t");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		new MySql().test();
+	}
+
+}
---
+++ src/org/code/helloworld/java/jdbc/oracle/Oracle.java
@@ -0,0 +1,68 @@
+package org.code.helloworld.java.jdbc.oracle;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
+/**
+ * 
+ * 描述： oracle 连接数据库测试
+ * 
+ * @author yanchangyou@gmail.com
+ * @date 2014-08-30 19:56:12
+ *
+ */
+public class Oracle {
+
+	public void test() throws Exception {
+
+		// 加载驱动
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+
+		// 数据库连接信息：url，用户名，密码
+		String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+		String userName = "p_soms_dev";
+		String password = "p_soms_dev";
+
+		// 获取连接
+		Connection conn = DriverManager.getConnection(url, userName, password);
+
+		// 创建语句对象
+		Statement stat = conn.createStatement();
+
+		// 查询语句
+		String sql = "select * from dual";
+
+		// 执行查询
+		ResultSet rs = stat.executeQuery(sql);
+		ResultSetMetaData rsMeta = rs.getMetaData();
+
+		// 输出列名称
+		int columns = rsMeta.getColumnCount();
+		for (int i = 0; i < columns; i++) {
+			System.out.print(rsMeta.getColumnName(i + 1) + "\t");
+		}
+
+		System.out.println();
+		System.out.println();
+
+		while (rs.next()) {// 遍历每一行
+
+			// 输出每行的列值
+			for (int i = 0; i < columns; i++) {
+				Object value = rs.getObject(i + 1);
+
+				// 输出每列的列值
+				System.out.print(value + "\t");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		new Oracle().test();
+	}
+
+}
